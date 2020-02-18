e01b4aec44d09b390f639d9add800e4aaae460be
JK <57945595+newmade01@users.noreply.github.com>
2020-02-19 02:47:29 +0900

    update

--- CoronaCal/app/src/main/java/com/example/coronacal/Example.java
+++ CoronaCal/app/src/main/java/com/example/coronacal/Example.java
@@ -1,4 +1,18 @@
 package com.example.coronacal;
 
-public class Example {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Example extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.example);
+
+
+
+    }
+
 }
