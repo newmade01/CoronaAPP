6c685640a2f335d39eacf56f1ab04dda39ccf104
JK <57945595+newmade01@users.noreply.github.com>
2020-02-19 00:32:25 +0900

    update

--- CoronaCal/app/src/main/java/com/example/coronacal/MainActivity.java
+++ CoronaCal/app/src/main/java/com/example/coronacal/MainActivity.java
@@ -9,7 +9,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,13 +19,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        Button btnNavTohorizontal = findViewById(R.id.btnGoTohorizontal);
-        btnNavTohorizontal.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, DBView.class));
-            }
-        });
+
 
 
         Button btnNavToSecond = findViewById(R.id.btnGoToSecondScreen);
