21a44e7186e264a822872c21754de919daf7aaca
JK <57945595+newmade01@users.noreply.github.com>
2020-02-20 01:10:24 +0900

    마지막 commit복원

--- CoronaCal/app/src/main/java/com/example/coronacal/LockActivity.java
+++ CoronaCal/app/src/main/java/com/example/coronacal/LockActivity.java
@@ -3,11 +3,7 @@ package com.example.coronacal;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.content.Intent;
-import android.os.Bundle;
-import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
