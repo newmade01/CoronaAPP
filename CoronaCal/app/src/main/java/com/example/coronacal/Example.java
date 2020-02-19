2939e0d4b14433460b0cdce0c24b1061d2527a8b
JK <57945595+newmade01@users.noreply.github.com>
2020-02-20 00:37:34 +0900

    update DB
    MSSQL 연동파일 아직 안올라감!

--- CoronaCal/app/src/main/java/com/example/coronacal/Example.java
+++ CoronaCal/app/src/main/java/com/example/coronacal/Example.java
@@ -1,153 +1,17 @@
 package com.example.coronacal;
 
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
-import android.app.ProgressDialog;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.URL;
-import java.net.URLConnection;
-import java.net.URLEncoder;
-
-import android.app.ProgressDialog;
-import android.os.Looper;
-import android.os.StrictMode;
-import android.os.Bundle;
-import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.Toast;
-import android.widget.ViewFlipper;
-
-import java.util.ArrayList;
-import java.util.List;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.ResponseHandler;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.BasicResponseHandler;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.ProgressDialog;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.List;
-
+import androidx.appcompat.app.AppCompatActivity;
 public class Example extends AppCompatActivity {
-
-    ViewFlipper Vf;
-    Button BtnSignIn, BtnSignUp;
-    EditText inputID, inputPW;
-    HttpPost httppost;
-    StringBuffer buffer;
-    HttpResponse response;
-    HttpClient httpclient;
-    List<NameValuePair> nameValuePairs;
-    ProgressDialog dialog = null;
-    TextView tv;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
-
-        BtnSignUp = (Button)findViewById(R.id.btn_signup);
-        BtnSignIn = (Button)findViewById(R.id.btn_signin);
-        inputID = (EditText)findViewById(R.id.user_id);
-        inputPW = (EditText)findViewById(R.id.user_pw);
-        tv = (TextView)findViewById(R.id.textView2);
-
-        BtnSignIn.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dialog = ProgressDialog.show(Example.this, "",
-                        "Validating user...", true);
-                new Thread(new Runnable() {
-                    public void run() {
-                        login();
-                    }
-                }).start();
-            }
-        });
-    }
-
-    void login() {
-        try {
-            httpclient = new DefaultHttpClient();
-            httppost = new HttpPost("http://C:/Apache24/htdocs/login.php");
-            nameValuePairs = new ArrayList<NameValuePair>(2);
-            nameValuePairs.add(new BasicNameValuePair("username", inputID.getText().toString()));
-            nameValuePairs.add(new BasicNameValuePair("password", inputPW.getText().toString()));
-            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-            response = httpclient.execute(httppost);
-            ResponseHandler<String> responseHandler = new BasicResponseHandler();
-            final String response = httpclient.execute(httppost, responseHandler);
-            System.out.println("Response : " + response);
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    tv.setText("Response from PHP : " + response);
-                    dialog.dismiss();
-                }
-            });
-
-            if (response.equalsIgnoreCase("User Found")) {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Toast.makeText(Example.this, "Login Success", Toast.LENGTH_SHORT).show();
-                    }
-                });
-
-                startActivity((new Intent(Example.this, MenuPage.class)));
-                finish();
-            } else {
-                Toast.makeText(Example.this, "Login Fail", Toast.LENGTH_SHORT).show();
-            }
-        }
-        catch(Exception e)
-        {
-            dialog.dismiss();
-            System.out.println("Exception : " + e.getMessage());
-        }
-    }
-
-    public void CliSignUp(View view)
-    {
-        Intent intent = new Intent(this, SignupPage.class);
-        startActivity(intent);
-    }
 }
-
-
-
-
-
